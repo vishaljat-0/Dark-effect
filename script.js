ba7d707e69ac21938eba214276907ba1e79fdd9b
jatvishal57@gmail.com <jatvishal57@gmail.com>
2025-11-30 23:10:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let main = document.querySelector("main");
+let x = 0;
+let y = 0;
+let targetSize = 100;
+let currentSize = 100;
+
+main.addEventListener("mousemove", (e) => {
+  x = e.clientX;
+  y = e.clientY;
+
+  targetSize = Math.floor(Math.random() * 255);
+});
+
+function animate() {
+  // smooth spotlight position
+  let currentX = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--x")
+  );
+  let currentY = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--y")
+  );
+
+  let newX = currentX + (x - currentX) * 0.1;
+  let newY = currentY + (y - currentY) * 0.1;
+
+  document.documentElement.style.setProperty("--x", newX + "px");
+  document.documentElement.style.setProperty("--y", newY + "px");
+
+  // smooth random size change
+  currentSize = currentSize + (targetSize - currentSize) * 0.05; git commit -m "first commit"
+  document.documentElement.style.setProperty("--shape", currentSize + "px");
+
+  requestAnimationFrame(animate);
+}
+
+animate();
